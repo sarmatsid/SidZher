766a5a47dd06b5955e4aec4f5994e9ce0fd03890
Alex Rionov <misterrio535@gmail.com>
2022-03-03 00:47:38 +0300

    Update cookies, update submodule

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const https = require('https'); // подключаем модуль https
 const fs = require('fs'); // подключаем модуль fs для обращения к файлам
 const fetch = require('node-fetch');
 const { stringify } = require('querystring'); // требуется для проверки captcha для приведения в строковый формат передаваемых пареметров
+const { nextTick } = require("process");
+const res = require("express/lib/response");
 app.use(cookieParser('secret key')); // сообщает об использовании cookie и их обработке
 app.use(bodyParser.json()); // сообщает системе, что мы хотим использовать json
 
@@ -206,6 +208,11 @@ app.get('/jsencrypt.min.js', (req, res) => { // подключаем jsencrypt.m
 //    if (loggedIn == false) return res.redirect('/');
 //    next();
 // });
+
+app.use(function (req, res, next) {
+   if (req.signedCookies === undefined || Object.keys(req.signedCookies).length === 0) return res.redirect('/')
+   next();
+});
 
 app.use(express.static('public')); // подключаем папку public для обращения к страницам внутри нее
 
